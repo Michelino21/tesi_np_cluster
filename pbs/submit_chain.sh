d0aa2869343cd7cee86475118bff8b27c4321491
MatteoZanetti775 <mattyzanetti21@gmail.com>
2026-09-25 23:39:46 +0200

    Smoke_Un-Shared

--- pbs/submit_chain.sh
+++ pbs/submit_chain.sh
@@ -36,7 +36,7 @@ MANIFEST="${RESULTS_DIR}/last_results.txt"
 # Ordine di esecuzione della catena.
 JOBS=(
 #    "gaussian_H1_highBref_imbalance_validation"
-"multidistribution_cluster_compliant"
+"multidistribution_cluster_shared_permutations"
 #    "unbalanced_multidistribution_cluster_compliant"
 #    "test_pipeline"
 )
